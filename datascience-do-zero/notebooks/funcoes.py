2b707f55071a5429ff4028a01921d8f0c1db8cce
Elen Vieira <elenv.vieiracorporativo@gmail.com>
2026-05-17 15:40:06 -0300

    Atualizações

--- datascience-do-zero/notebooks/funcoes.py
+++ datascience-do-zero/notebooks/funcoes.py
@@ -15,4 +15,24 @@ x = aplly_to_one(my_double)  # igual a 2
 # também é fácil criar várias funções anônimas, as lambdas: 
 y = aplly_to_one(lambda x: x + 4) # igual a 5
 
-# você pode 
+# você pode atribuir lambdas a váriasveis, embora quase todo mundo recomende o def: 
+another_double = lambda x: 2 * x # não faça isso
+
+def another_double(x):
+    """faça isso"""
+    return 2 * x
+
+# os parâmetros da função também podem receber argumentos padrão, que devem ser especificados se você quiser obter um valor diferente do padrão: 
+def my_print(message = "my default message"):
+    print(message)
+
+my_print("Hello")    # imprime 'hello'
+my_print()           # imprime 'my default message'
+
+# as vezes é útil especificar argumentos pelo nome: 
+def full_name(first = "what is her name", last = "something"):
+    return first + " " + last
+
+full_name("Elen", "Vieira") # "Elen Vieira"
+full_name("Elen")           # "Elen Something"
+full_name(last = "Vieira")  # "What is her name Vieira"
